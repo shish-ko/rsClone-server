ef1a05bddbe1c11fa739ff2bd1586bed8cdd813e
shish-ko <andrey.shishko@gmail.com>
2023-02-03 14:05:57 +0300

    feat: registration

--- authController.js
+++ authController.js
@@ -1,11 +1,21 @@
-const User = require('./models/User')
+const User = require('./models/User');
+const bcrypt = require ('bcrypt');
 
 class AuthController {
   async register (req, res) {
     try {
-      res.send('Hello!')
+      
+      const {username, password} = req.body;
+      const candidate = await User.findOne({username})
+      if(candidate) {
+        return res.send(400).json("Username is exist");
+      }
+      const hashedPassword = await bcrypt.hash(password, 4);
+      const user = new User({username, password: hashedPassword});
+      await user.save();
+      res.json({message: 'User was added'})
     } catch (e) {
-
+      console.log(e)
     }
   }
 
