45e59c9a319c741838f43956e41babbe67709607
shish-ko <andrey.shishko@gmail.com>
2023-02-08 14:19:23 +0300

    fix: CORS policy

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { json } = require('express');
 const express = require('express');
 const mongoose = require ('mongoose');
+const cors = require('cors')
 
 const authRouter = require('./auth/authRouter');
 const scoreRouter = require('./score/scoreRouter');
 const customRouter = require('./custom_games/customRouter');
-const PORT = 3000;
+const PORT = 4000;
 
 
 const app = express();
 app.use(json());
+app.use(cors({origin: "http://localhost:3000"}));
 app.use("/auth", authRouter);
 app.use("/score", scoreRouter);
 app.use("/custom", customRouter);
